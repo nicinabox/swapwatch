05cb643e63909790efd8ea7720081800e17be79e
Nic Aitch <nic@nicinabox.com>
2017-12-29 22:01:26 -0600

    Fix reset

--- components/Head.js
+++ components/Head.js
@@ -8,7 +8,7 @@ export default () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet" />
     <link href="/static/reflex.min.css" rel="stylesheet" />
     <link href="/static/main.css" rel="stylesheet" />
 
