8b3429095db47e546450fc9be7bc0ebece5aaf21
Nic Aitch <nic@nicinabox.com>
2017-12-29 22:30:58 -0600

    Add footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="grid">
+          <div className="col-auto">
+            <p className="text-muted text-small text-center">
+              Made with all the ❤ in the world by <a href="https://twitter.com/nicinabox">@nicinabox</a>.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import {
 
 import Head from '../components/Head'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import Menu from '../components/Menu'
 import Search from '../components/Search'
 import Posts from '../components/Posts'
@@ -84,6 +85,8 @@ export class App extends React.Component {
             </div>
           </div>
         </div>
+
+        <Footer />
       </div>
     )
   }
