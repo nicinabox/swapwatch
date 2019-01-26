16e29412331db8ccb7b0dbf2abb2ed0226e3f0b1
Nic Haynes <nic@nicinabox.com>
2019-01-25 22:37:46 -0600

    Change server to express and fix rendering in firefox

--- server.js
+++ server.js
@@ -2,46 +2,39 @@
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
 
-const { createServer } = require('http')
-const { parse } = require('url')
+const express = require('express')
+const request = require('request')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-const PORT = process.env.PORT || 3000
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+const port = parseInt(process.env.PORT, 10) || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const redirects = [
-  { from: '/', to: '/mechmarket' },
-]
-
-const whitelistPaths = [
-  '/',
-  '/favicon.ico'
-]
-
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  const server = express()
 
-    redirects.forEach(({ from, to, type = 301 }) => {
-      if (pathname === from) {
-        res.writeHead(type, {
-          'Location': to
-        })
-        res.end()
-      }
-    })
+  server.use('/r*', (req, res) => {
+    const url = 'https://reddit.com/' + req.originalUrl
+    req.pipe(request(url)).pipe(res)
+  })
 
-    if (!whitelistPaths.includes(pathname)) {
-      app.render(req, res, '/', query)
-    } else {
-      handle(req, res, parsedUrl)
+  server.get('/', (req, res) => res.redirect(301, '/mechmarket'))
+
+  server.get('/:subreddit/:flair?', (req, res) => {
+    app.render(req, res, '/', req.query)
+  })
+
+  server.get('*', (req, res) => {
+    handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) {
+      throw err
     }
 
-  }).listen(PORT, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:' + PORT)
+    console.log(`> Ready on port ${port} [${env}]`)
   })
 })
